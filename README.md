b18a3cce9e5c3d346445c69c8e8767d42ed24cd0
DenzelD1 <118927034+DenzelD1@users.noreply.github.com>
2025-01-21 19:34:41 +0000

    leerArchivo

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Ev2
 
 Denzel Martin Delgado Urquieta, 21.401.250-2, ITI
+
+- Ejecutar el programa
+    g++ main.cpp -o main.exe
+    ./main.exe
--- main.cpp
+++ main.cpp
@@ -10,31 +10,45 @@ class Arbol {
 };
 
 vector<vector<int>> leerArchivo(string nombreArchivo) {
-    vector<vector<int>> matriz;
+    vector<vector<int>> vacio;
     ifstream archivo(nombreArchivo);
     if(!archivo.is_open()) {
         cout << "No se ha logrado abrir el archivo" << endl;
-        return matriz;
+        return vacio;
+    } else if(archivo.peek() == EOF) {
+        cout << "El archivo esta vacio" << endl;
+        return vacio;
     }
-    int linea1 = 0;
+
     int numNodos;
     string linea;
-    while(getline(archivo, linea)) {
-        linea1++;
+    if(getline(archivo, linea)) {
         stringstream ss(linea);
+        ss >> numNodos;
+    }
 
-        if(linea1 == 1) {
-            ss >> numNodos;
+    vector<vector<int>> matriz(numNodos, vector<int>(numNodos, 0));
+
+    int lineas = 0;
+    while(getline(archivo, linea)) {
+        stringstream ss(linea);
+        for(int i = 0; i < numNodos; i++) {
+            ss >> matriz[lineas][i];
             ss.ignore();
         }
-        
+        lineas++;
     } archivo.close();
-    cout << numNodos << endl;
+
     return matriz;
 }
 
 int main() {
-    cout << "Nodos leidos actualmente" << endl;
     vector<vector<int>> matriz = leerArchivo("matriz.txt");
+
+    for(int i = 0; i < matriz.size(); i++) {
+        for(int j = 0; j < matriz.size(); j++) {
+            cout << matriz[i][j] << "  ";
+        } cout << endl;
+    }
     return 0;
 }
