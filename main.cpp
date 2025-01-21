3292fedb665760fa4e0c95341632b35d916ea4f1
DenzelD1 <118927034+DenzelD1@users.noreply.github.com>
2025-01-21 16:14:25 +0000

    pruebaArchivo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <climits>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
+class Arbol {
+
+};
+
+vector<vector<int>> leerArchivo(string nombreArchivo) {
+    vector<vector<int>> matriz;
+    ifstream archivo(nombreArchivo);
+    if(!archivo.is_open()) {
+        cout << "No se ha logrado abrir el archivo" << endl;
+        return matriz;
+    }
+    int linea1 = 0;
+    int numNodos;
+    string linea;
+    while(getline(archivo, linea)) {
+        linea1++;
+        stringstream ss(linea);
+
+        if(linea1 == 1) {
+            ss >> numNodos;
+            ss.ignore();
+        }
+        
+    } archivo.close();
+    cout << numNodos << endl;
+    return matriz;
+}
+
 int main() {
-    cout << "asdga" << endl;
+    cout << "Nodos leidos actualmente" << endl;
+    vector<vector<int>> matriz = leerArchivo("matriz.txt");
     return 0;
 }
